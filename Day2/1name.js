a7ca12bd613b9027c9b21237f32f749fb858c05d
VINANK <neupaneayush3@gmail.com>
2023-01-12 17:14:28 +0545

    module

---
+++ Day2/1name.js
@@ -0,0 +1,5 @@
+var name=function(){
+	return("my name is ayush neupane");		//this is returned by the function expression
+}
+
+module.exports=name;						//exporting the function name so that other files can use it
---
+++ Day2/2call.js
@@ -0,0 +1,3 @@
+const name=require('./1name');	//requiring the module in file 1name.js
+
+console.log(name());			//calling the function of another module
