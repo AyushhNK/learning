26f32364b95cf3f542a38d3aad4bea42be1062ed
VINANK <neupaneayush3@gmail.com>
2023-01-19 15:34:49 +0545

    templating engine(ejs)

--- Day9/app.js
+++ Day9/app.js
@@ -6,7 +6,7 @@ const app=express();
 app.set('view engine','ejs');		//setting the view engine to ejs
 
 app.get('/profile/:username',(req,res)=>{
-	var data={age:20,occupation:'developer'};						//temporaray database
+	var data={age:20,occupation:'developer',hobbies:['eating','playing','music']};						
 	res.render("profile",{person:req.params.username,data:data});	//rendering the data in profile.ejs in views folder
 });
 app.listen(3000,()=>{
