a25184dd70400d7f35da06ec902419272796cf25
VINANK <neupaneayush3@gmail.com>
2023-01-19 18:00:44 +0545

    partial template

--- Day9/app.js
+++ Day9/app.js
@@ -9,6 +9,9 @@ app.get('/profile/:username',(req,res)=>{
 	var data={age:20,occupation:'developer',hobbies:['eating','playing','music']};						
 	res.render("profile",{person:req.params.username,data:data});	//rendering the data in profile.ejs in views folder
 });
+app.get('/contact',(req,res)=>{
+	res.render("contact");
+})
 app.listen(3000,()=>{
 	console.log('server running at port 3000');
 });
