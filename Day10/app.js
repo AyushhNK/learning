94ff639be60c93216a83197eb2bf8968597dacaf
VINANK <neupaneayush3@gmail.com>
2023-01-20 16:39:22 +0545

    middleware and static files

---
+++ Day10/app.js
@@ -0,0 +1,19 @@
+//middleware
+const express=require('express');
+
+const app=express();
+
+app.use('/assets',express.static('assets'));	//using the middleware
+
+app.set('view engine','ejs');		//setting the view engine to ejs
+
+app.get('/profile',(req,res)=>{
+	var data={age:20,occupation:'developer',hobbies:['eating','playing','music']};						
+	res.render("profile",{data:data});	//rendering the data in profile.ejs in views folder
+});
+app.get('/contact',(req,res)=>{
+	res.render("contact");
+})
+app.listen(3000,()=>{
+	console.log('server running at port 3000');
+});
