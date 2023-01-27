be1401bf2bb5f265b31fb6a2ebd59ec4a0ec0fe0
VINANK <neupaneayush3@gmail.com>
2023-01-27 15:24:59 +0545

    fs revise

---
+++ revise1/appendfile.js
@@ -0,0 +1,5 @@
+const fs=require('fs');
+
+// fs.writeFileSync('note.txt',"my name is ayush");	//for making and writing in a file
+
+fs.appendFileSync('note.txt',"i live in Nepal");	//for adding the content in a file
