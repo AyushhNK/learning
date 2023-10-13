dd4fc25abfb687e2fabcd2125fb7e1aab9a9c678
VINANK <neupaneayush3@gmail.com>
2023-10-13 17:44:29 +0545

    dyanamic values in template

--- React/blog/src/App.jsx
+++ React/blog/src/App.jsx
@@ -3,11 +3,23 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const title="Welcome to the blog!!";
+  const likes=50;
+  {/*//objects are not valid in react
+    // const obj={name:"ayush",age:10}
+    // */}
+    const link="http://www.google.com";
+
 
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>{likes}</p>
+        <p>{Math.random()*10}</p>
+        <a href={link}>Google link</a>
+
+
       </div>
     </div>
   )
