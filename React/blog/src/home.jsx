b77283f73c8d1626cee84bf95fe02340b27ff5f4
VINANK <neupaneayush3@gmail.com>
2023-10-15 16:45:01 +0545

    click event

--- React/blog/src/home.jsx
+++ React/blog/src/home.jsx
@@ -1,7 +1,15 @@
 const Home=()=>{
+	const handleClick=(e)=>{
+		console.log("hello world",e.target);
+	}
+	const handleClickAgain=(name)=>{
+		console.log("hello"+name);
+	}
 	return(
 		<div className="home">
 			<h2>Home page</h2>
+			<button onClick={handleClick}>click me</button>
+			<button onClick={()=>handleClickAgain("ayush")}>click me again</button>
 		</div>
 		);
 }
--- React/blog/src/navbar.jsx
+++ React/blog/src/navbar.jsx
@@ -4,11 +4,7 @@ const Navbar= () =>{
 			<h1>The Dojo Blog</h1>
 			<div className="links">
 				<a href="/">Home</a>
-				<a href="/create" style={{
-					color:"white",
-					backgroundColor:"#f1356d",
-					borderRadius:"8px"
-				}}>New Blog</a>
+				<a href="/create">New Blog</a>
 			</div>
 		</nav>
 		);
