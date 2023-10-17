69f80d5e7143309795b635e9deba3787c671dec0
VINANK <neupaneayush3@gmail.com>
2023-10-17 17:17:53 +0545

    functions as props

--- React/blog/src/bloglist.jsx
+++ React/blog/src/bloglist.jsx
@@ -1,6 +1,7 @@
 const BlogList=(props)=>{
 	const blogs=props.blogs;
 	const title=props.title;
+	const handleDelete=props.handleDelete;
 	return(
 		<div className="blog-list">
 			<h1>{title}</h1>
@@ -8,6 +9,7 @@ const BlogList=(props)=>{
 					<div className="blog-preview" key={blog.id}>
 					<h1>{blog.title}</h1>
 					<p>{blog.body}</p>
+					<button onClick={()=>handleDelete(blog.id)}>Delete Blog</button>
 					</div>
 
 					))}
--- React/blog/src/home.jsx
+++ React/blog/src/home.jsx
@@ -7,12 +7,18 @@ const Home=()=>{
 		{body:"i am ayush",title:"ayushname",author:"ayush",id:0},
 		{body:"i am hari",title:"hariname",author:"hari",id:1},
 		{body:"i am sita",title:"sitaname",author:"sita",id:2}])
+
+	const handleDelete=(id)=>{
+		const newBlog=blogs.filter(blog=>blog.id!=id);
+		setBlogs(newBlog);
+
+	}
 		
 
 	
 	return(
 		<div className="home">
-			<BlogList blogs={blogs} title="All Blogs"/>
+			<BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
 
 		</div>
 		);
