4f10fe7ee4fd91c36e66a5f1ea7eb218f35e4a8f
VINANK <neupaneayush3@gmail.com>
2023-10-31 17:32:16 +0545

    handling fetch error

--- React/blog/src/home.jsx
+++ React/blog/src/home.jsx
@@ -5,20 +5,29 @@ const Home=()=>{
 
 	const [blogs,setBlogs]=useState(null)
 	const [isPending,setIsPending]=useState(true);
-
+	const [error,setError]=useState()
 		
 	useEffect(()=>{
-		fetch("http://localhost:8000/blogs").then(res=>{
+		fetch("http://localhost:8000/blogss").then(res=>{
+			console.log(res)
+			if(!res.ok){
+				throw Error('could not fetch data for that resource')
+			}
 			return res.json();
 		}).then(data=>{
 			setBlogs(data)
 			setIsPending(false);
-		});
+			setError(null)
+		}).catch(err=>{
+			setIsPending(false)
+			setError(err.message)
+		})
 	},[]);
 
 	
 	return(
 		<div className="home">
+			{error && <div>{error}</div>}
 			{isPending && <div>loading...</div>}
 			{blogs && <BlogList blogs={blogs} title="All Blogs"/>}
 			
